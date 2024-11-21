3a4ff43694733a94616d17240377e978c645c0fc
Suresh <kvsuresh@gmail.com>
2024-11-21 14:44:41 +0530

    prompt improvements

---
+++ blank-nextjs-app/app/components/ThankYou.js
@@ -0,0 +1,8 @@
+export default function ThankYou() {
+    return (
+        <div className={styles.thankYou}>
+            <h2>Thank You</h2>
+            <p>Your order has been confirmed!</p>
+        </div>
+    );
+}
